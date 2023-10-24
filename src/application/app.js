02c2d33957d9af5604230ef08755dff2fcc8536a
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2023-10-24 20:57:59 +0700

    cors

--- src/application/app.js
+++ src/application/app.js
@@ -2,9 +2,11 @@ import express from "express";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { publicRouter } from "../routes/public.js";
 import { userRouter } from "../routes/protected.js";
+import cors from "cors";
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(publicRouter);
 app.use(userRouter);
