79388cdc827c2260966050371c45bcd3a5a919ca
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2024-01-24 23:41:11 +0700

    add auth page

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import { Aside } from "@/components/aside/aside";
+import { Main } from "@/components/home/main";
+import Layout from "@/components/layout/main-layout";
+import { Sidebar } from "@/components/sidebar/sidebar";
+
+export default function Home() {
+  return (
+    <Layout>
+      <Main />
+    </Layout>
+  );
+}
