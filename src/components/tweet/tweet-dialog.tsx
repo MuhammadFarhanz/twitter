e48af59e9089fa262e30f3922ca6127608edb2b7
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2024-02-05 20:48:31 +0700

    refactor

---
+++ src/components/tweet/tweet-dialog.tsx
@@ -0,0 +1,67 @@
+import React, { ChangeEvent, useRef, useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { CustomIcon } from "../ui/custom-icon";
+import { Textarea } from "../ui/textarea";
+import { ScrollArea } from "../ui/scroll-area";
+import TextareaInput from "./input/textarea-input";
+import ImageComponent from "./ui/image-preview";
+import { handleFileChange } from "./utils/handleChangeFile";
+import ImageInput from "./input/image-input";
+import { AvatarProfile } from "./ui/avatar";
+import useTweetFormLogic from "./service/useTweetForm";
+import { TweetFormDialog } from "./input/tweet-form";
+import FormHeader from "./input/dialog-header";
+
+function Tweetdialog() {
+  const [open, setOpen] = useState(false);
+
+  const handleSuccess = () => {
+    setOpen(!open);
+  };
+
+  const { formik, handleFileInputChange } = useTweetFormLogic({
+    onSuccess: handleSuccess,
+  });
+  const handleDeleteImage = (index: number) => {
+    const updatedImages = [...formik.values.images];
+    updatedImages.splice(index, 1);
+    formik.setFieldValue("images", updatedImages);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <button className="custom-button rounded-full p-3  main-tab accent-tab absolute right-4 -translate-y-[130px] bg-main-accent text-lg font-bold outline-none transition hover:brightness-90 active:brightness-75 xs:static xs:translate-y-0 xs:hover:bg-main-accent/90 xs:active:bg-main-accent/75 xl:w-11/12">
+          <CustomIcon className=" h-6 w-6 xl:hidden" iconName="FeatherIcon" />
+          <p
+            className="hidden xl:block 
+          text-white"
+          >
+            Tweet
+          </p>
+        </button>
+      </DialogTrigger>
+
+      <DialogContent className="bg-main-background flex border-0 p-2 max-h-[90%] top-[5%]">
+        <ScrollArea className="max-h-full w-full">
+          <FormHeader onClose={() => formik.resetForm()} />
+          <TweetFormDialog
+            formik={formik}
+            handleFileInputChange={handleFileInputChange}
+            handleDeleteImage={handleDeleteImage}
+          />
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default Tweetdialog;
