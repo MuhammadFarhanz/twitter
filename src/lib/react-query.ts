99b2aa178de9a65b0f9cef5a55fab5a35901dc19
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2024-02-19 18:05:48 +0700

    add react query

---
+++ src/lib/react-query.ts
@@ -0,0 +1,28 @@
+import { DefaultOptions, QueryCache, QueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { toast } from "sonner";
+
+const queryConfig: DefaultOptions = {
+  queries: {
+    refetchOnWindowFocus: false,
+    retry: false,
+  },
+};
+
+export const queryClient = new QueryClient({
+  defaultOptions: queryConfig,
+  queryCache: new QueryCache({
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        const err = error as AxiosError;
+
+        if (err.response && err.response.status >= 500) {
+          toast.error("Server error");
+          return;
+        }
+      }
+
+      toast.error("Something went wrong");
+    },
+  }),
+});
