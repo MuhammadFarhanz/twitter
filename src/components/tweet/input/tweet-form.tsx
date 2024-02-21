3c84d22fe8f84b5270fd8e55eec5d193e25c93de
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2024-02-21 22:40:57 +0700

    add tweet form

---
+++ src/components/tweet/input/tweet-form.tsx
@@ -0,0 +1,77 @@
+import { forwardRef, useRef } from "react";
+import { AvatarProfile } from "../ui/avatar";
+import TextareaInput from "./textarea-input";
+import ImageComponent from "../ui/image-preview";
+import ImageInput from "./image-input";
+import { cn } from "@/lib/utils";
+import TweetButton from "../ui/tweet-button";
+
+interface TweetFormProps {
+  formik: any;
+  handleFileInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleDeleteImage: (index: number) => void;
+  className?: any;
+}
+
+export const TweetFormDialog = ({
+  formik,
+  handleFileInputChange,
+  handleDeleteImage,
+  className,
+}: TweetFormProps) => {
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  const handleTextareaInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (ref.current) {
+      ref.current.style.height = "auto";
+      ref.current.style.height = `${e.target.scrollHeight - 16}px`;
+    }
+  };
+
+  return (
+    <form
+      onSubmit={formik.handleSubmit}
+      className="bg-main-background flex flex-col"
+      encType="multipart/form-data"
+    >
+      <label
+        className={cn("hover-animation grid w-full gap-3 px-2 py-3", className)}
+      >
+        <div className="flex flex-row ">
+          <AvatarProfile className="mr-3" />
+
+          <div className="flex w-full flex-col mt-1 min-h-[134px] h-fit">
+            <TextareaInput
+              ref={ref}
+              onInput={handleTextareaInput}
+              maxLength={500}
+              value={formik.values.content}
+              onChange={formik.handleChange}
+            />
+
+            <div
+              className={`items-center w-full mt-2 grid${
+                formik.values.images.length > 1 && " grid-cols-2"
+              } gap-2`}
+            >
+              {formik.values.images.map((image: any, index: any) => (
+                <ImageComponent
+                  key={index}
+                  image={image}
+                  index={index}
+                  imageLength={formik.values.images.length}
+                  onDeleteImage={handleDeleteImage}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-between border-t border-light-border dark:border-dark-border pt-3">
+          <ImageInput handleFileInputChange={handleFileInputChange} />
+          <TweetButton />
+        </div>
+      </label>
+    </form>
+  );
+};
