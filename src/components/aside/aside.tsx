c20eb39763942f5f2b27a56f3f9e021e5f742948
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2024-04-24 22:57:03 +0700

    refactor: completed aside

--- src/components/aside/aside.tsx
+++ src/components/aside/aside.tsx
@@ -6,14 +6,12 @@ import { ScrollArea } from "../ui/scroll-area";
 import Trending from "./trends";
 import FollowButton from "../user/follow-button";
 import UserCard from "./user-card";
-import { useGetRandomUser } from "@/api/getRandomUser";
+import { useGetRandomUser } from "@/api/useGetRandomUser";
+import Link from "next/link";
 
 export function Aside(): JSX.Element {
-  const { data: user } = useGetUser();
-
   const { data } = useGetRandomUser();
 
-  console.log(data, "asu");
   const nav = [
     "Terms of Service",
     "Privacy policy",
@@ -25,10 +23,9 @@ export function Aside(): JSX.Element {
   ];
 
   return (
-    // <ScrollArea className="h-screen rounded-md border">
-    <div className="w-96 min-h-screen flex-col gap-4  hidden lg:flex ">
+    <div className="w-96 min-h-screen flex-col gap-4 hidden lg:flex ">
       <div className=" sticky -top-1/2 max-w-[380px] w-full px-4">
-        <div className="fixed w-[350px] bg-black py-2">
+        <div className="fixed w-[350px] bg-main-background py-2">
           <div className="bg-blue-0 sticky top-0">
             <div className="">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -43,13 +40,6 @@ export function Aside(): JSX.Element {
              px-3 py-1 text-sm shadow-sm focus-visible:ring-1 transition-colors bg-main-search-background
               file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50`}
               />
-              {/* <input
-              type="search"
-              id="search"
-              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search"
-              required
-            /> */}
             </div>
           </div>
         </div>
@@ -78,13 +68,17 @@ export function Aside(): JSX.Element {
           <Trending />
         </div>
 
-        <div className="flex flex-col  pt-3 mt-6 rounded-xl bg-main-sidebar-background  ">
+        <div className="flex flex-col pt-3 mt-4 rounded-xl bg-main-sidebar-background  ">
           <h1 className="text-[20px] px-4 leading-[20px] font-[901]  py-2 text-light-primary dark:text-dark-primary">
             Who to follow
           </h1>
           <div className="h-full">
             {data?.map((item: any) => {
-              return <UserCard user={item} />;
+              return (
+                <Link href={`/${item?.username}`}>
+                  <UserCard user={item} />
+                </Link>
+              );
             })}
           </div>
           <p className="px-4 h-14 flex items-center text-main-accent cursor-pointer dark:hover:bg-dark-primary/10 hover:bg-light-primary/10 text-[15px] leading-[20px]">
@@ -93,6 +87,5 @@ export function Aside(): JSX.Element {
         </div>
       </div>
     </div>
-    // </ScrollArea>
   );
 }
