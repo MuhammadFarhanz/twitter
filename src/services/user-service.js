1490789f516f8c3099a87143274418973c67c30a
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2024-05-10 18:09:11 +0700

    sadafsaafa

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -82,7 +82,7 @@ const login = async (request, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: true,
+      // httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "none",
       maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
