1487ef7ede292f84ff9309b12db7e2b229fa898a
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2024-10-19 03:03:19 +0700

    fix: middleware redirect too many

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest, res: NextResponse) {
-  const token = req.cookies.get("token");
+export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
 
+  if (pathname.startsWith("/_next/static") || pathname === "/favicon.ico") {
+    return NextResponse.next();
+  }
+
+  if (pathname === "/auth/sign-in") {
+    return NextResponse.next();
+  }
+
+  const token = req.cookies.get("token");
   if (!token) {
     return NextResponse.redirect(new URL("/auth/sign-in", req.url));
   }
 
-  const response = NextResponse.next();
-
-  response.headers.set(`x-middleware-cache`, `no-cache`);
-  return response;
+  return NextResponse.next();
 }
 
 export const config = {
